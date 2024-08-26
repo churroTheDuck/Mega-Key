60b983c3576925e5137fd645c796690bf09041ca
Lang Chen <langchenca@gmail.com>
2024-08-25 17:16:54 -0700

    Initial Commit

---
+++ Mega Key/Mega_KeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mega_KeyApp.swift
+//  Mega Key
+//
+//  Created by Lang Chen on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Mega_KeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
